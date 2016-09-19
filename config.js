321159d96267e502f9bf5a865c4591a8d6d06c22
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2016-09-18 19:28:30 -0700

    add default config

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  "helloworld": { // "password": "opensesame", "salt": "F6zh2FiMANg"
+    "iv":"KunbE6NUDE3Nq96XfgOeww==",
+    "v":1,
+    "iter":10000,
+    "ks":256,
+    "ts":64,
+    "mode":"ccm",
+    "adata":"",
+    "cipher":"aes",
+    // Remove salt
+    "ct":"MTL6BLCgFpuovpybzVzzGN7CKsa17yDGYb8QGjN01qmH7NPuyYyUea7nS80Kzxw6mE8Ln8ePeHli1Z/hsMlEEuJiEZCd8RM="},
+};
